7776259af2f630f12b15053f10865b154d196c87
Lcole.engr <lcole.engr@gmail.com>
2020-01-19 01:35:18 -0500

    established database connection using mongoose/mongo

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
 
 // Import Routes
 
 const authRoute = require("./routes/auth");
 
+// Establish database connection
+mongoose.connect("mongodb://localhost/bonappetit",
+ { useNewUrlParser: true },
+ () => console.log("Connection to DB Successful!")
+  );
+
+
+// Middleware
+app.use(express.json());
 
 
 // Route Middleware
